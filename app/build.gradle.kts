f52660ac3616e5bbfcc5c2ec2bf3893d1a7eb845
hjaber1974-create <hjaber1974@gmail.com>
2026-09-17 22:29:19 +0000

    Release Hassan Garage 4.1 navigation fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.hassangarage.v4"
         minSdk = 24
         targetSdk = 35
-        versionCode = 40
-        versionName = "4.0"
+        versionCode = 41
+        versionName = "4.1"
     }
 
     compileOptions {
