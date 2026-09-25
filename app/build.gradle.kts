429b6b25606079de29efdb573cac05c15445729a
hjaber1974-create <hjaber1974@gmail.com>
2026-09-25 22:44:38 +0000

    Bump Hassan Garage final version to 5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.hassangarage.final"
         minSdk = 24
         targetSdk = 35
-        versionCode = 100
-        versionName = "4.1-final"
+        versionCode = 500
+        versionName = "5.0-final"
     }
 
     compileOptions {
